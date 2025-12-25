18f1438ea4a9ec1f8c90f5b1577dae65c008ce23
KorayTemizkan <temizkankoray1923@gmail.com>
2025-12-25 13:09:08 +0300

    Profilde kaydedilen urunleri gostermeye calismak(su an hatali)

--- knitting_app/lib/views/profile_view/profile_view.dart
+++ knitting_app/lib/views/profile_view/profile_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:knitting_app/controllers/app_bar.dart';
+import 'package:knitting_app/controllers/providers/product_provider.dart';
+import 'package:knitting_app/controllers/providers/shared_preferences_provider.dart';
+import 'package:knitting_app/models/guide_model.dart';
+import 'package:provider/provider.dart';
 
 class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
@@ -9,10 +13,56 @@ class ProfileView extends StatefulWidget {
 }
 
 class _ProfileViewState extends State<ProfileView> {
+  List<int> savedIds = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadSavedIds();
+  }
+
+  Future<void> _loadSavedIds() async {
+    final sharedPreferencesProvider = context.read<SharedPreferencesProvider>();
+    final stringList = await sharedPreferencesProvider
+        .finishGetSavedCharacters();
+
+    setState(() {
+      savedIds = stringList
+          .map((e) => int.tryParse(e))
+          .whereType<int>()
+          .toList();
+    }); // setstate kullanıyoruz çünkü bu dosyadaki savedID's değeri değişecek
+  }
+
+  /// 🔹 savedIds sırasına göre product.id eşleştirir
+  List<ProductModel> _getSavedProducts({
+    required List<int> savedIds,
+    required List<ProductModel> products,
+  }) {
+    final productMap = {for (final p in products) p.id: p};
+
+    return savedIds
+        .map((id) => productMap[id])
+        .whereType<ProductModel>()
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBarWidget(title: 'KnittingApp - Profil'),
+    final products = context.watch<ProductProvider>().products;
+
+    if (products.isEmpty) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
+    final savedProducts = _getSavedProducts(
+      savedIds: savedIds,
+      products: products,
+    );
+
+    return Scaffold(
+      appBar: AppBarWidget(title: 'KnittingApp - Profil'),
       body: Center(
         child: Column(
           children: [
@@ -20,8 +70,33 @@ class _ProfileViewState extends State<ProfileView> {
             Text('kullanıcı kartı'),
             Text('kaydedilenler'),
             Text('beğenilenler'),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: savedCharactersList.length,
+                itemBuilder: (context, index) {
+                  final productIndex = intSavedCharactersList[index];
+                  final p = products[productIndex];
+
+                  return Card(
+                    child: ListTile(
+                      leading: Image.network(
+                        p.imageUrl,
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.cover,
+                      ),
+
+                      title: Text(p.title),
+                      subtitle: Text("${p.difficulty}, ${p.estimatedHour}"),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
-    );  }
+    );
+  }
 }
